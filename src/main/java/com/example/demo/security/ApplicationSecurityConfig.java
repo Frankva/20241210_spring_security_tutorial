a2b65dde44dea57d78ee94f6ba5e0ee527a0fdc6
Frankva <24254885+Frankva@users.noreply.github.com>
2024-12-10 13:56:48 +0100

    feat: Ant Matchers

--- src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
@@ -14,7 +14,16 @@ public class ApplicationSecurityConfig {
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());
+        http
+            .authorizeHttpRequests((requests) -> requests
+                .requestMatchers("/api/**")
+                .authenticated()
+                .requestMatchers("/", "index", "/css/*", "/js/*")
+                .permitAll()
+                .anyRequest()
+                .permitAll()
+            );
+
         http.httpBasic(withDefaults());
         return http.build();
     }
